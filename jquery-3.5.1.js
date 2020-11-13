4248b3fca0718000d8ad4a5f4567b906e6e8b53e
kimheesu88 <70933898+kimheesu88@users.noreply.github.com>
2020-11-13 12:28:19 +0900

    Create jquery-3.5.1.js

---
+++ jquery-3.5.1.js
@@ -0,0 +1,12 @@
+<div id="container">
+       <select name="select_box" id="select_box" >
+           <option value="">Select</option>
+           <option value="">option1</option>
+           <option value="">option2</option>
+           <option value="">option3</option>
+           <option value="">option4</option>
+           <option value="">option5</option>
+           <option value="">option6</option>
+       </select>
+       <input type="hidden" name="select_flag" id="select_flag" value="0">
+</div>
